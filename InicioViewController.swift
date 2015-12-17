9cbe4ba920cbe29e3bbe725e810bee123d0b52de
senaingrid <iss@ingridsena.com.br>
2015-12-17 11:23:00 -0200

    Reorganização de tudo
    Código, classes e visual. Warnings zerado.

---
+++ InicioViewController.swift
@@ -0,0 +1,24 @@
+//
+//  InicioViewController.swift
+//  Bora
+//
+//  Created by Student on 12/17/15.
+//  Copyright © 2015 BoraApps. All rights reserved.
+//
+
+import UIKit
+
+class InicioViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+        
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+        
+        
+}
